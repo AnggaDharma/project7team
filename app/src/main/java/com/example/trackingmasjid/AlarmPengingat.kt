7faa18a9aa136b93c2edc2565f7fcebabb07e472
Fiki Nur Rahman <fiki.nr13@gmail.com>
2023-10-16 21:37:21 +0700

    c

--- app/src/main/java/com/example/trackingmasjid/AlarmPengingat.kt
+++ app/src/main/java/com/example/trackingmasjid/AlarmPengingat.kt
@@ -7,5 +7,6 @@ class AlarmPengingat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_pengingat)
+        cdjhcdhcdhc
     }
 }
