abcaf75082ca9c44cbb1e88bf1126fe4c8bf6d56
Fiki Nur Rahman <fiki.nr13@gmail.com>
2023-10-16 23:12:40 +0700

    perubahan

--- app/src/main/java/com/example/trackingmasjid/AlarmActivity.kt
+++ app/src/main/java/com/example/trackingmasjid/AlarmActivity.kt
@@ -3,9 +3,9 @@ package com.example.trackingmasjid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Donasi : AppCompatActivity() {
+class AlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donasi)
+        setContentView(R.layout.activity_alarm_pengingat)
     }
 }
--- app/src/main/java/com/example/trackingmasjid/ZakatProfesiActivity.kt
+++ app/src/main/java/com/example/trackingmasjid/ZakatProfesiActivity.kt
@@ -3,9 +3,9 @@ package com.example.trackingmasjid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AlarmPengingat : AppCompatActivity() {
+class ZakatProfesiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm_pengingat)
+        setContentView(R.layout.activity_zakat_profesi)
     }
 }
