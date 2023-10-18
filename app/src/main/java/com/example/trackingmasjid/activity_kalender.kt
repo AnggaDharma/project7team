6f0c77ccf4028e0abd6260b09c2fecab04894162
Fiki Nur Rahman <fiki.nr13@gmail.com>
2023-10-19 03:20:50 +0700

    update tampilan

--- app/src/main/java/com/example/trackingmasjid/activity_kalender.kt
+++ app/src/main/java/com/example/trackingmasjid/activity_kalender.kt
@@ -2,10 +2,19 @@ package com.example.trackingmasjid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.Toast
 
 class activity_kalender : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalender)
+
+        val calendarView = findViewById<CalendarView>(R.id.calender)
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Lakukan sesuatu dengan tanggal yang dipilih
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+            Toast.makeText(this, "Tanggal dipilih: $selectedDate", Toast.LENGTH_SHORT).show()
+        }
     }
 }
