172bc668b4199a5c779a4f52247e711fc4c1b84e
Jogy Gunawan <jogygunawan@gmail.com>
2023-10-18 23:49:17 +0900

    update donasi,kalender,solat

---
+++ app/src/main/java/com/example/trackingmasjid/activity_detail_donasi.kt
@@ -0,0 +1,11 @@
+package com.example.trackingmasjid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_detail_donasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_donasi)
+    }
+}
